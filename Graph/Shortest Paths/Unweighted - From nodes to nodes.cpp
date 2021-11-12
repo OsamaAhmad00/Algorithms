40607b772828b7856b54fe841749759b53b5b377
Osama Ahmad <ssssss1233345@gmail.com>
2021-11-13 00:57:06 +0200

    Improved GraphShortestUnweightedPathsFromNodesToNodes.cpp

--- Graph/Shortest Paths/Unweighted - From nodes to nodes.cpp
+++ Graph/Shortest Paths/Unweighted - From nodes to nodes.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <algorithm>
 
+const int UNVISITED = -1;
+const int NO_PARENT = -2;
 typedef std::vector<std::vector<int>> Graph;
 
 std::vector<std::vector<int>> calc_shortest_paths (
@@ -14,7 +16,7 @@ std::vector<std::vector<int>> calc_shortest_paths (
     // this only works with unweighted graphs.
 
     std::vector<std::vector<int>> result;
-    std::vector<int> parent_of(graph.size(), -1);
+    std::vector<int> parent_of(graph.size(), UNVISITED);
 
     std::vector<bool> is_end_node(graph.size(), false);
     for (auto node : ending_nodes) {
@@ -23,41 +25,33 @@ std::vector<std::vector<int>> calc_shortest_paths (
 
     std::queue<int> queue;
     for (auto node : starting_nodes) {
-        // -1 means not visited. -2 means is a starting node.
-        // shouldn't set a parent for a starting node..
-        parent_of[node] = -2;
+        parent_of[node] = NO_PARENT;
         queue.push(node);
     }
 
     while (!queue.empty())
     {
-        int level_size = queue.size();
+        int node = queue.front();
+        queue.pop();
 
-        while (level_size--)
-        {
-            int node = queue.front();
-            queue.pop();
+        for (int child : graph[node]) {
 
-            for (int child : graph[node]) {
+            if (parent_of[child] == UNVISITED) {
+                parent_of[child] = node;
+                queue.push(child);
+            }
 
-                if (parent_of[child] == -1) {
-                    // not visited
-                    parent_of[child] = node;
-                    queue.push(child);
+            // adding a path only through its parent
+            // to avoid having duplicate equal paths
+            if (is_end_node[child] && parent_of[child] == node) {
+                std::vector<int> path;
+                int current_node = child;
+                while (current_node >= 0) {
+                    path.push_back(current_node);
+                    current_node = parent_of[current_node];
                 }
-
-                // adding a path only through its parent
-                // to avoid having duplicate equal paths
-                if (is_end_node[child] && parent_of[child] == node) {
-                    std::vector<int> path;
-                    int current_node = child;
-                    while (current_node >= 0) {
-                        path.push_back(current_node);
-                        current_node = parent_of[current_node];
-                    }
-                    std::reverse(path.begin(), path.end());
-                    result.push_back(path);
-                }
+                std::reverse(path.begin(), path.end());
+                result.push_back(path);
             }
         }
     }
