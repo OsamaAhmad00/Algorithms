61771d8d6cc26cad1b68c7f5a4525004b03db45b
Osama Ahmad <ssssss1233345@gmail.com>
2022-01-06 20:05:14 +0200

    Improved TreeCentersByShrinking.cpp

--- Graph/Tree/Diameters and Centers/TreeCentersByShrinking.cpp
+++ Graph/Tree/Diameters and Centers/TreeCentersByShrinking.cpp
@@ -1,27 +1,11 @@
-#include <map>
-#include <set>
 #include <queue>
 #include <vector>
 #include <iostream>
-#include <functional>
-#include <algorithm>
 
-struct Edge
+typedef std::vector<std::vector<int>> Tree;
+
+std::pair<int, int> get_center_nodes_by_shrinking_tree(const Tree& tree)
 {
-    int to;
-    int weight;
-
-    bool operator<(const Edge &other) const
-    {
-        return to < other.to;
-    }
-};
-
-typedef std::vector<std::set<Edge>> Tree;
-
-std::pair<int, int> get_center_nodes_by_shrinking_tree(Tree tree)
-{
-    // NOTE: this function assumes that tree[0] is not used.
     // some optimizations can be done here to be faster.
 
     // using a queue to ensure that old leaves
@@ -35,31 +19,36 @@ std::pair<int, int> get_center_nodes_by_shrinking_tree(Tree tree)
         }
     }
 
-    // you shouldn't manipulate tree.size() directly
-    //  (erase nodes randomly) since this will affect
-    //  the indices of the nodes in the tree.
-    // the 0-th index is not used here.
-    int tree_size = tree.size() - 1;
+    // since we don't delete nodes from the tree,
+    // the size of the tree will not change. we'll
+    // make a copy of the size, and each time we
+    // process a node, we'll decrement the size by 1.
+    int tree_size = tree.size();
 
-    // 1 or 2 nodes left as leafs means that there
-    // are 1 or 2 center nodes respectively.
+    // instead of actually deleting edges going to
+    // children, we can just keep track of how many
+    // children are left for each node.
+    std::vector<int> child_count(tree.size());
+    for (int i = 0; i < tree.size(); i++)
+        child_count[i] = tree[i].size();
+
+    // if 2 nodes remain, then
+    // the tree has 2 centers.
     while (tree_size > 2)
     {
         int leaves_count = leaves.size();
         while (leaves_count--)
         {
-            auto node_idx = leaves.front();
+            int node = leaves.front();
             leaves.pop();
 
-            auto &node = tree[node_idx];
-            auto &only_edge = *node.begin();
-            auto parent_idx = only_edge.to;
-            auto weight = only_edge.weight;
+            // since the current node is a leaf, it'll
+            // have a single edge, going to its parent.
+            int parent = tree[node].front();
 
-            auto &parent = tree[parent_idx];
-            parent.erase({node_idx, weight});
-            if (parent.size() == 1) {
-                leaves.push(parent_idx);
+            int& children = child_count[parent];
+            if ((--children) == 1) {
+                leaves.push(parent);
             }
 
             tree_size--;
@@ -76,60 +65,60 @@ std::pair<int, int> get_center_nodes_by_shrinking_tree(Tree tree)
     return {res1, res2};
 }
 
-void add_child(Tree &tree, int parent, int child, int weight)
+void add_child(Tree &tree, int parent, int child)
 {
-    tree[parent].insert({child, weight});
-    tree[child].insert({parent, weight});
+    tree[parent].push_back(child);
+    tree[child].push_back(parent);
 }
 
 // Tree 1 and 2 are isomorphic.
 
 Tree get_sample_tree1()
 {
-    Tree tree(8);
+    Tree tree(7);
 
-    add_child(tree, 1, 2, 1);
-    add_child(tree, 1, 3, 2);
+    add_child(tree, 0, 1);
+    add_child(tree, 0, 2);
 
-    add_child(tree, 2, 6, 3);
-    add_child(tree, 2, 7, 4);
+    add_child(tree, 1, 5);
+    add_child(tree, 1, 6);
 
-    add_child(tree, 3, 4, 5);
-    add_child(tree, 3, 5, 6);
+    add_child(tree, 2, 3);
+    add_child(tree, 2, 4);
 
     return tree;
 }
 
 Tree get_sample_tree2()
 {
-    Tree tree(8);
+    Tree tree(7);
 
-    add_child(tree, 7, 6, 1);
-    add_child(tree, 7, 5, 2);
+    add_child(tree, 6, 5);
+    add_child(tree, 6, 4);
 
-    add_child(tree, 6, 2, 3);
-    add_child(tree, 6, 1, 4);
+    add_child(tree, 5, 1);
+    add_child(tree, 5, 0);
 
-    add_child(tree, 5, 4, 5);
-    add_child(tree, 5, 3, 6);
+    add_child(tree, 4, 3);
+    add_child(tree, 4, 2);
 
     return tree;
 }
 
 Tree get_sample_tree3()
 {
-    Tree tree(9);
+    Tree tree(8);
 
-    add_child(tree, 1, 2, 1);
-    add_child(tree, 1, 3, 2);
+    add_child(tree, 0, 1);
+    add_child(tree, 0, 2);
 
-    add_child(tree, 2, 6, 3);
-    add_child(tree, 2, 7, 4);
+    add_child(tree, 1, 5);
+    add_child(tree, 1, 6);
 
-    add_child(tree, 3, 4, 5);
-    add_child(tree, 3, 5, 6);
+    add_child(tree, 2, 3);
+    add_child(tree, 2, 4);
 
-    add_child(tree, 4, 8, 7);
+    add_child(tree, 3, 7);
 
     return tree;
 }
