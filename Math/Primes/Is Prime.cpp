5a39bfdef5188a7e67692d92877fc2cd59b298f6
Osama Ahmad <ssssss1233345@gmail.com>
2022-05-14 01:02:18 +0200

    Added Math/Primes/Is Prime.cpp

---
+++ Math/Primes/Is Prime.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+template <typename T>
+bool is_prime(const T& number)
+{
+    if (number % 2 == 0)
+        return false;
+
+    T s = std::sqrt(std::abs(number));
+    for (T i = 3; i <= s; i+=2)
+        if (number % i == 0)
+            return false;
+    return true;
+}
+
+int main()
+{
+    std::cout << std::boolalpha;
+    std::cout << is_prime(1) << std::endl;
+    std::cout << is_prime(-11) << std::endl;
+    std::cout << is_prime(132) << std::endl;
+    std::cout << is_prime(13) << std::endl;
+}
