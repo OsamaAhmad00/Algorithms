05ce66eb3662054be3619098d7d676f4b9a541e4
Osama Ahmad <ssssss1233345@gmail.com>
2022-06-10 21:47:02 +0200

    Added Modular Arithmetic/Reduce Large Powers (Using Euler's Totient Function).cpp

---
+++ Math/Modular Arithmetic/Reduce Large Powers (Using Euler's Totient Function).cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+
+template <typename T>
+T GCD(T a, T b)
+{
+    while (a != 0) {
+        b %= a;
+        std::swap(a, b);
+    }
+
+    return b;
+}
+
+template <typename T, typename U, typename V>
+T fast_mod_power(T number, U power, V mod)
+{
+    T result = 1;
+    while (power > 0)
+    {
+        if (power % 2 != 0)
+            result = (result * number) % mod;
+        number = (number * number) % mod;
+        power /= 2;
+    }
+
+    return (result + mod) % mod;
+}
+
+template <typename T>
+T phi_prime_in_number(T& number, const T& prime)
+{
+    T power_of_prime = 1;
+    while (number % prime == 0) {
+        power_of_prime *= prime;
+        number /= prime;
+    }
+
+    return (power_of_prime / prime) * (prime - 1);
+}
+
+template <typename T>
+T phi(T number)
+{
+    T result = (number > 1);
+
+    if (number % 2 == 0)
+        result *= phi_prime_in_number(number, 2);
+
+    for (T i = 3; i * i <= number; i+=2)
+        if (number % i == 0)
+            result *= phi_prime_in_number(number, i);
+
+    if (number != 1)
+        result *= phi_prime_in_number(number, (T)number);
+
+    return result;
+}
+
+template <typename T>
+T reduced_fast_mod_power(const T& number, const T& power, const T& mod)
+{
+    /*
+     * Returns 0 if GCD(number, mod) != 1.
+     *
+     * Let a = number, p = power, n = mod.
+     *
+     * Euler's Theorem:
+     *  If GCD(a, n) = 1, then a^Phi(n) ≡ 1 (mod n) where
+     *  Phi is Euler's Totient Function.
+     * Notice that Phi(p) = p - 1 if p is a prime number.
+     *  This means that if the mod is a prime number,
+     *  a^(p - 1) ≡ 1 (mod n).
+     *
+     * Claim: We can reduce a^p to a^(p % x).
+     * Proof:
+     *  Let x = Phi(power). We can represent a^p % n as
+     *  a^((p/x * x) + p%x) = a^(p/x * x) * a^(p % x)
+     *  = 1 * a^(p % x) = a^(p % x).
+     *
+     * NOTICE THAT PHI(0) = PHI(1) = 0, THUS, THE MODS
+     *  MUST BE >= 2 TO AVOID DIVISION BY 0 EXCEPTION.
+     */
+
+    if (std::abs(GCD(number, mod)) != 1)
+        return 0;
+
+    return fast_mod_power(number, power % phi(mod), mod);
+}
+
+
+void test(int number, int power, int mod)
+{
+    int result1 = reduced_fast_mod_power(number, power, mod);
+
+    if (std::abs(GCD(number, mod)) != 1) {
+        if (result1 != 0)
+            std::cout << "Result should be 0" << std::endl;
+        return;
+    }
+
+    int result2 = fast_mod_power(number, power, mod);
+
+    if (number != 0 && result1 != result2)
+        std::cout << "The result is not correct" << std::endl;
+}
+
+int main()
+{
+    int n = 200;
+    for (int mod = 2; mod <= n; mod++) {
+        for (int number = 0; number < mod; number++) {
+            for (int power = 0; power <= n; power++) {
+                test(number, power, mod);
+                test(-number, power, mod);
+            }
+        }
+    }
+}
